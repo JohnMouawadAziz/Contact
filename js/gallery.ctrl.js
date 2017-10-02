d2eea0140b3cb12cb03e03312da6ad47d8e3a02d
john <john.mouawad.aziz@gmail.com>
2017-10-03 00:11:23 +0300

    first commit

---
+++ js/gallery.ctrl.js
@@ -0,0 +1,76 @@
+contactGallery.controller('galleryController', ['$scope', function () {
+   $scope.listProducts=[
+
+	{id:'m1' , name:'john' , email:'hhh@jjj.com' , quantety:'01234514334' },
+	{id:'m2' , name:'bola' , email:'hhh@jjj.com' , quantety:'01234514334' },
+	{id:'m3' , name:'shery' , email:'hhh@jjj.com' , quantety:'01234514334' },
+	{id:'m4' , name:'monica' , email:'hhh@jjj.com' , quantety:'01234514334' }
+
+	];
+
+///////////////////////Add function///////////////////////
+
+
+	$scope.add=function(){
+
+		$scope.listProducts.push({
+
+			id:$scope.id , name:$scope.name , email:$scope.email , quantety:$scope.quantety
+
+		});
+
+		$scope.id='';
+		$scope.name='';
+		$scope.email='';
+		$scope.quantety='';
+	};
+
+/////////////////////// save Edit function///////////////////////
+
+
+	$scope.edit=function (){
+		var index=getSelectedIndex($scope.id);
+		$scope.listProducts[index].name = $scope.name;
+		$scope.listProducts[index].email = $scope.email;
+		$scope.listProducts[index].quantety = $scope.quantety;
+
+	};
+
+///////////////////////Edit function///////////////////////
+
+	$scope.selectEdit=function(id){
+
+		var index=getSelectedIndex(id);
+		var product = $scope.listProducts[index];
+		$scope.id=product.id;
+		$scope.name=product.name;
+		$scope.email=product.email;
+		$scope.quantety=product.quantety;
+	};
+
+
+
+///////////////////////Delete function///////////////////////
+
+	$scope.del = function(id){
+		var result=confirm('are you sure ?!');
+		if(result===true){
+
+			var index=getSelectedIndex(id);
+			$scope.listProducts.splice(index , 1);
+		}
+
+	};
+
+
+	function getSelectedIndex(id){
+
+		for(var i=0 ; i<$scope.listProducts.length ; i++)
+			if($scope.listProducts[i].id==id)
+				return i;
+			return -1;
+
+		}
+
+	
+}]);
---
+++ js/login.ctrl.js
@@ -0,0 +1,44 @@
+contactGallery.controller('loginController', ['$scope', '$timeout', function ($scope, $timeout) {
+    $scope.login = {
+        userName: '',
+        password: ''
+    };
+    $scope.loginAuth = loginAuth;
+
+
+
+    $timeout(function () {
+        $('.ui.form')
+            .form({
+                fields: {
+                    userName: {
+                        identifier: 'userName',
+                        rules: [{
+                            type: 'empty',
+                            prompt: 'Please enter a username'
+                        }]
+                    },
+                    password: {
+                        identifier: 'password',
+                        rules: [{
+                                type: 'empty',
+                                prompt: 'Please enter a password'
+                            },
+                            {
+                                type: 'minLength[6]',
+                                prompt: 'Your password must be at least {ruleValue} characters'
+                            }
+                        ]
+                    },
+
+                },
+                onSuccess: function () {
+                    $scope.loginAuth();
+                }
+            });
+    }, 200);
+
+    function loginAuth() {
+        console.log($scope.login);
+    }
+}]);
